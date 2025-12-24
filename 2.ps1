37b7279f06d300eef4a814f2d76d3aceb05e397d
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-12-24 12:26:14 -0600

    switch statement

--- 2.ps1
+++ 2.ps1
@@ -1,8 +1,7 @@
-$PokemonNum = 23232
-if ($PokemonNum -ge 0 -and $PokemonNum -le 50) {
-    Write-Output "You have caught $PokemonNum Pokémon."
-} elseif ($PokemonNum -gt 50 -and $PokemonNum -le 100) {
-    Write-Output "impressive $PokemonNum Pokémon."
-} else {
-    Write-Output "god level $PokemonNum Pokémon."
+$House = "Stark"
+Switch($House) {
+    "Stark" { "Winter is Coming" }
+    "Lannister" { "Hear Me Roar" }
+    "Targaryen" { "Fire and Blood" }
+    Default { "Unknown House" }
 }
