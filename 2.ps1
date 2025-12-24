1a96c129e5046809b1a77fe7e36c8830d624e819
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-12-24 12:24:51 -0600

    pokemon stuff

--- 2.ps1
+++ 2.ps1
@@ -1,16 +1,8 @@
-
-try {
-[Int32]$PokemonCaught = Read-Host "Enter the number of Pokémon you have caught"
-
-if ($PokemonCaught -eq 908) {
-    Write-Output "Congratulations! You have caught all 908 Pokémon!"
+$PokemonNum = 23232
+if ($PokemonNum -ge 0 -and $PokemonNum -le 50) {
+    Write-Output "You have caught $PokemonNum Pokémon."
+} elseif ($PokemonNum -gt 50 -and $PokemonNum -le 100) {
+    Write-Output "impressive $PokemonNum Pokémon."
 } else {
-    Write-Output "You have caught $PokemonCaught Pokémon. Keep going to catch them all!"
-}
-
-} catch {
-    Write-Error "An error occurred: $_"
-}
-finally {
-    Write-Host "Thank you for using the Pokémon Catch Tracker!" -ForegroundColor Magenta
+    Write-Output "god level $PokemonNum Pokémon."
 }
