37f1c71e5e2d8e078ba5458bc9fe64e35c3087b4
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2025-12-24 12:40:24 -0600

    while loop, for

--- loop.ps1
+++ loop.ps1
@@ -1,5 +1,25 @@
 $Name = "Nani"
 
-for ($i = 0; $i -lt 5; $i++) {
+for ($i = 0; $i -lt 2; $i++) {
     Write-Output "Iteration $i - Hello, $Name!"
 }
+
+
+$Xmen = @("Wolverine", "Cyclops", "Storm", "Jean Grey", "Professor X")
+foreach ($hero in $Xmen) {
+    Write-Output "X-Men Member: $hero"
+}
+
+
+$counter = 0
+while ($counter -ne 6) {
+    Write-Output $Xmen[$counter]
+    $counter++
+}
+
+
+$counter = $Xmen.Count - 1
+do {
+    Write-Output $Xmen[$counter]
+    $counter--
+} while ($counter -ge 0) 
